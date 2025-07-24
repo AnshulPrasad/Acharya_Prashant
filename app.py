1fb713b50923f1205e103aa788458dad5c626ffd
Anshulprasad <anshulprasad5440@gmail.com>
2025-07-24 11:10:18 +0530

    fix syntax error.

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ async def ask_question(request: Request):
         response = generate_response(query, limit_context)
 
         logging.info(f"Received query: {query}")
-        logging.info(f"Context: {" ".join(limit_context.split('\n'))}")
+        logging.info(f"Context: {' '.join(limit_context.split('\n'))}")
         logging.info(f"Response: {response}")
 
         return JSONResponse({"answer": response})
