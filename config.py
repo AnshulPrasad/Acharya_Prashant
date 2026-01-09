018624b8dcb1494bd4bc67614d973a50faa588e0
Anshul Prasad <anshulprasad5440.com>
2026-01-09 14:57:30 +0530

    string to pathlib.Path shifting

--- config.py
+++ config.py
@@ -1,23 +1,19 @@
 import os
+from pathlib import Path
 
 CHANNEL_URLS = [
     "https://www.youtube.com/@AcharyaPrashant",
-    # "https://www.youtube.com/@ShriPrashant",
-    # "https://www.youtube.com/@AP_Shastragyaan_Hindi",
-    # "https://www.youtube.com/@AP_lalkaar",
-    # "https://www.youtube.com/@AP_RashtraDharma_Hindi",
-    # "https://www.youtube.com/@AP_Shakti_Hindi",
-    # "https://www.youtube.com/@AP_Prakrati_Hindi",
+    "https://www.youtube.com/@ShriPrashant",
 ]
 
-VTT_DIR = "data/subtitles_vtt"
-TXT_DIR = "data/transcripts_txt"
-FILE_PATHS = "data/file_paths.pkl"
-TRANSCRIPTS = "data/transcripts.pkl"
+VTT_DIR = Path("data/subtitles_vtt")
+TXT_DIR = Path("data/transcripts_txt")
+FILE_PATHS = Path("data/file_paths.pkl")
+TRANSCRIPTS = Path("data/transcripts.pkl")
 TRANSCRIPT_INDEX = "data/transcript_index.faiss"
-RETRIEVED_TRANSCRIPTS_FILE = "outputs/retrieved_transcripts.txt"
-RESPONSE_FILE = "outputs/generated_response.txt"
-COOKIES_FILE = "utils/youtube_cookies.txt"
+RETRIEVED_TRANSCRIPTS_FILE = Path("outputs/retrieved_transcripts.txt")
+RESPONSE_FILE = Path("outputs/generated_response.txt")
+COOKIES_FILE = Path("utils/youtube_cookies.txt")
 API_URL = "https://models.github.ai/inference"
 HF_API_TOKEN = os.environ.get("HF_API_TOKEN")
 GH_API_TOKEN = os.environ.get("GITHUB_TOKEN")
