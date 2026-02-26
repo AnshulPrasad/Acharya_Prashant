eaba29c94be9cddcb33e322a4f27ca60e1cc61bf
Anshul Prasad <anshulprasad5440.com>
2026-02-26 15:05:22 +0530

    enable verbose to find erro.

--- api/generate_response.py
+++ api/generate_response.py
@@ -17,7 +17,7 @@ def load_model_at_startup():
         llm = Llama.from_pretrained(
             repo_id="microsoft/Phi-3-mini-4k-instruct-gguf",
             filename="Phi-3-mini-4k-instruct-q4.gguf",
-            verbose=False,
+            verbose=True,
             n_gpu_layers=0,  # CPU only (safe for HF Spaces)
             n_ctx=4096,
         )
